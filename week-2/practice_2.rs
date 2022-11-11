16d32326d075be81c1e75e293ac2e020c20d7dd1
chisom74 <117773454+chisom74@users.noreply.github.com>
2022-11-11 13:04:15 +0100

    Add files via upload

---
+++ week-2/practice_2.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!(); //prints just a
+
+	println!("Hello "); //prints hello
+
+	println!("format {} arguements", "some"); //prints format some arguements
+
+	println!("My name is {}. I am the class rep of stream 1", "Chisom Onyemachi");
+}
